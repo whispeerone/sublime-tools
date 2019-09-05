8849b322d0b1aa596fd822adf693f8948bc25531
whispeer_ <44951541+whispeerone@users.noreply.github.com>
2019-09-05 15:17:15 +0300

    Update smart-console-log.py

--- smart-console-log.py
+++ smart-console-log.py
@@ -8,9 +8,10 @@ class ExampleCommand(sublime_plugin.TextCommand):
 
 		currentLineRaw = self.view.substr(self.view.line(self.view.sel()[0]))
 
+
 		self.configureCursor(currentLineRaw)
 
-		variable = self.findTarget(currentLineRaw.strip())
+		variable = self.findTarget(currentLineRaw)
 		text = self.prepareText(variable)
 		self.printText(text)
 
@@ -29,14 +30,12 @@ class ExampleCommand(sublime_plugin.TextCommand):
 	def findTarget(self, lineRaw):
 		line = lineRaw.strip();
 
-		match = re.match(r'(var|const|let)?( )?(.*)( )?=', line)
+		target = self.findVaribleAssignment(line)
 
-		if match != None:
-			return match.group(3)
-		else:
-			print("wooow") # TODO
-			return
+		if target == None:
+			target = self.findVariableUsing(lineRaw)
 
+		return target
 
 	def prepareText(self, variable):
 		text = "console.log(\"{0}\"); // TODO: do not forget to delete".format(variable)
@@ -45,7 +44,44 @@ class ExampleCommand(sublime_plugin.TextCommand):
 		return text;
 
 	def configureCursor(self, line):
+		# TODO if return at first place of line
+		self.setCursorToEndOfLine(line)
+
+	def setCursorToEndOfLine(self, line):
+		self.setCursorColumnPosition(len(line))
+
+	def setCursorColumnPosition(self, col):
 		cursorPosition = self.view.rowcol(self.view.sel()[0].begin())
-		newCursonPosition = self.view.text_point(cursorPosition[0], len(line))
+		newCursonPosition = self.view.text_point(cursorPosition[0], col)
+		self.setCursorPosition(newCursonPosition)
+
+	def setCursorPosition(self, point):
 		self.view.sel().clear()
-		self.view.sel().add(sublime.Region(newCursonPosition))
+		self.view.sel().add(sublime.Region(point))
+
+	def findVaribleAssignment(self, line):
+		match = re.match(r'(var|const|let)?( )?(.*)( )?=', line)
+
+		if match != None:
+			return match.group(3)
+		else:
+			return None
+
+	def findVariableUsing(self, line):
+		
+		bracketIndex = line.find("(")
+
+		if bracketIndex == -1:
+			return None
+
+		self.setCursorColumnPosition(bracketIndex + 1)
+
+		self.view.run_command('expand_selection', {"to": "brackets"})
+
+		sel = self.view.sel()
+		result = self.view.substr(sel[0]) 
+		print(result)
+
+		self.setCursorToEndOfLine(line)
+
+		return result
